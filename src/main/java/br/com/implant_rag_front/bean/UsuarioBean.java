8b68066ec7d37b030bd42c3c0108dd29c76709c1
Josenilton Carvalho de Sousa <nilton.carvalho@boavista.local>
2021-12-14 14:56:48 -0300

    Finalizando login

--- src/main/java/br/com/implant_rag_front/bean/UsuarioBean.java
+++ src/main/java/br/com/implant_rag_front/bean/UsuarioBean.java
@@ -10,22 +10,20 @@ import br.com.implant_rag_front.dto.UsuarioDTO;
 import br.com.implant_rag_front.service.UsuarioService;
 import br.com.implant_rag_front.util.JsfUtil;
 
-@ManagedBean(name="MBUsuario")
+@ManagedBean(name = "MBUsuario")
 @ViewScoped
 public class UsuarioBean {
-	
+
 	private UsuarioDTO usuario;
-	
+
 	private ArrayList<UsuarioDTO> itens;
-	
+
 	@ManagedProperty(value = "#{usuarioService}")
 	private UsuarioService usuarioService;
-	
-	
-	
+
 	public UsuarioBean() {
 		this.usuario = new UsuarioDTO();
-		
+
 	}
 
 	public UsuarioDTO getUsuario() {
@@ -43,30 +41,30 @@ public class UsuarioBean {
 	public void setItens(ArrayList<UsuarioDTO> itens) {
 		this.itens = itens;
 	}
-	
+
 	public void validarUsuario() {
-		
+
 	}
-	
+
 	public void validarSenha() {
-		
+
 	}
 
 	public void setUsuarioService(UsuarioService usuarioService) {
 		this.usuarioService = usuarioService;
-	}	
-	
+	}
+
 	public String validaLogin() {
-		
+
 		Boolean validadorLogin = this.usuarioService.validar(this.usuario.getEmail(), this.usuario.getSenha());
-		
-		if(validadorLogin == true) {
+
+		if (validadorLogin == true) {
 			JsfUtil.adicionarMensagemDeSucesso("Login realizado com sucesso");
 			return "cadastroOperadoras";
-		}else if(validadorLogin == false) {
-			JsfUtil.adicionarMensagemDeErro("Usuário ou senha inválido");		
-}
-		return null ;
+		} else if (validadorLogin == false) {
+			JsfUtil.adicionarMensagemDeErro("Usurio ou senha invlido");
+		}
+		return null;
 	}
-	
+
 }
